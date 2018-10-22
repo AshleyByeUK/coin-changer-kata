47613d5f97608ba247b2fc865ded00cba45ab9db
Ashley Bye <ashley+git@ashleybye.uk>
2018-10-22 10:35:39 +0100

    correct 6 pence

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -5,13 +5,20 @@ class CoinChanger
     elsif [200, 100, 50, 20, 10, 5, 2, 1].include?(amount)
       [amount]
     else
-      coins = []
-      number_of_coins = amount / 2
-      add_coins(coins, 2, number_of_coins)
-      number_of_coins = amount % 2
-      add_coins(coins, 1, number_of_coins)
-      coins
+      count_coins(amount)
     end
+  end
+
+  def count_coins(amount)
+    coins = []
+      [5, 2, 1].each do |coin|
+        if amount / coin > 0
+          number_of_coins = amount / coin
+          add_coins(coins, coin, number_of_coins)
+          amount = amount % coin
+        end
+      end
+      coins
   end
 
   def add_coins(current_coins, coin, number)
