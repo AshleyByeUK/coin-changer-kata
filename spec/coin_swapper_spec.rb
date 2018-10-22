80646904caed404be862dd563e2160dbbc872577
Ashley Bye <ashley+git@ashleybye.uk>
2018-10-22 12:26:12 +0100

    take 3 - 2p

--- spec/coin_swapper_spec.rb
+++ spec/coin_swapper_spec.rb
@@ -4,9 +4,14 @@ require 'coin_swapper'
 describe CoinSwapper do
   context 'with the specified amount' do
     it 'returns a list of coins' do
-      swapper = CoinSwapper.new()
-      expect(swapper.swap(0)).to eq [0]
-      expect(swapper.swap(1)).to eq [1]
+      assert_swaps(0, [0])
+      assert_swaps(1, [1])
+      assert_swaps(2, [2])
     end
   end
+end
+
+def assert_swaps(amount, coins)
+  swapper = CoinSwapper.new()
+  expect(swapper.swap(amount)).to eq coins
 end
