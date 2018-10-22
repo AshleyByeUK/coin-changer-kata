3b2e75ba9924c84c3cea90003104850ce9d3ac57
Ashley Bye <ashley+git@ashleybye.uk>
2018-10-22 11:43:23 +0100

    take 2 - 2p

--- spec/coin_exchanger_spec.rb
+++ spec/coin_exchanger_spec.rb
@@ -5,8 +5,14 @@ describe CoinExchanger do
   context "with the specified amount" do
     it "returns a list of coins" do
       changer = CoinExchanger.new()
-      expect(changer.exchange(0)).to eq [0]
-      expect(changer.exchange(1)).to eq [1]
+      assert_exchanges(0, [0])
+      assert_exchanges(1, [1])
+      assert_exchanges(2, [2])
     end
+  end
+
+  def assert_exchanges(amount, change)
+    changer = CoinExchanger.new()
+    expect(changer.exchange(amount)).to eq change
   end
 end
