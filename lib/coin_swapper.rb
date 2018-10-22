6f433cb80514b2ff1f750f89ace1fd5192cf8747
Ashley Bye <ashley+git@ashleybye.uk>
2018-10-22 12:54:17 +0100

    final refactor

--- lib/coin_swapper.rb
+++ lib/coin_swapper.rb
@@ -1,16 +1,22 @@
 class CoinSwapper
+  COINS = [200, 100, 50, 20, 10, 5, 2, 1]
+
   def swap(amount)
     if amount > 0
-      coins = []
-      [200, 100, 50, 20, 10, 5, 2, 1].each do |coin|
-        while amount >= coin
-          amount -= coin
-          coins.push(coin)
-        end
-      end
-      coins
+      compute_coins_for_swap(amount)
     else
       [0]
     end
+  end
+
+  private def compute_coins_for_swap(amount)
+    coins = []
+    COINS.each do |coin|
+      while amount >= coin
+        amount -= coin
+        coins.push(coin)
+      end
+    end
+    coins
   end
 end
