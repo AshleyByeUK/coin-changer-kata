68c1316072eba50e895c68c5c2ae9b17ca67f875
Ashley Bye <ashley+git@ashleybye.uk>
2018-10-22 12:09:24 +0100

    take 2 - 388p

--- lib/coin_exchanger.rb
+++ lib/coin_exchanger.rb
@@ -2,7 +2,7 @@ class CoinExchanger
   def exchange(amount)
     coins = []
     if amount > 0
-      [50, 20, 10, 5, 2, 1].each do |coin|
+      [200, 100, 50, 20, 10, 5, 2, 1].each do |coin|
         while amount >= coin
           coins.push(coin)
           amount -= coin
