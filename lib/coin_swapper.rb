79b6d29cadc3f48141b30eba740eb30942072a50
Ashley Bye <ashley+git@ashleybye.uk>
2018-10-22 12:36:58 +0100

    take 3 - 5p

--- lib/coin_swapper.rb
+++ lib/coin_swapper.rb
@@ -2,6 +2,10 @@ class CoinSwapper
   def swap(amount)
     if amount > 0
       coins = []
+      while amount >= 5
+        amount -= 5
+        coins.push(5)
+      end
       while amount >= 2
         amount -= 2
         coins.push(2)
--- spec/coin_swapper_spec.rb
+++ spec/coin_swapper_spec.rb
@@ -9,6 +9,7 @@ describe CoinSwapper do
       assert_swaps(2, [2])
       assert_swaps(3, [2, 1])
       assert_swaps(4, [2, 2])
+      assert_swaps(5, [5])
     end
   end
 end
