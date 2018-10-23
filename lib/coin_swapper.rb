a91df718505a5017341a170c72a4b9108d186ec3
Ashley Bye <ashley+git@ashleybye.uk>
2018-10-23 13:21:25 +0100

    refactor

--- lib/coin_swapper.rb
+++ lib/coin_swapper.rb
@@ -2,14 +2,6 @@ class CoinSwapper
   COINS = [200, 100, 50, 20, 10, 5, 2, 1].freeze
 
   def swap(amount)
-    coins = [0] unless amount > 0
-    coins = compute_coins_for_swap(amount) if amount > 0
-    coins
-  end
-
-  private
-
-  def compute_coins_for_swap(amount)
     coins = []
     COINS.each do |coin|
       while amount >= coin
@@ -20,3 +12,5 @@ class CoinSwapper
     coins
   end
 end
+
+# Enumerable module, use inject.
--- spec/coin_swapper_spec.rb
+++ spec/coin_swapper_spec.rb
@@ -2,22 +2,20 @@ require 'rspec'
 require 'coin_swapper'
 
 describe CoinSwapper do
-  context 'with the specified amount' do
-    it 'returns a list of coins' do
-      assert_swaps(0, [0])
-      assert_swaps(1, [1])
-      assert_swaps(2, [2])
-      assert_swaps(3, [2, 1])
-      assert_swaps(4, [2, 2])
-      assert_swaps(5, [5])
-      assert_swaps(88, [50, 20, 10, 5, 2, 1])
-      assert_swaps(98, [50, 20, 20, 5, 2, 1])
-      assert_swaps(388, [200, 100, 50, 20, 10, 5, 2, 1])
-    end
+  it 'returns a list of coins' do
+    assert_swaps(0, [])
+    assert_swaps(1, [1])
+    assert_swaps(2, [2])
+    assert_swaps(3, [2, 1])
+    assert_swaps(4, [2, 2])
+    assert_swaps(5, [5])
+    assert_swaps(88, [50, 20, 10, 5, 2, 1])
+    assert_swaps(98, [50, 20, 20, 5, 2, 1])
+    assert_swaps(388, [200, 100, 50, 20, 10, 5, 2, 1])
   end
 end
 
 def assert_swaps(amount, coins)
-  swapper = CoinSwapper.new()
+  swapper = CoinSwapper.new
   expect(swapper.swap(amount)).to eq coins
 end
